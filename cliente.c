120dbaadc3a26e87d2f6350bcefed62522935ec2
adrianjmejias <adrianjmejias@gmail.com>
2018-04-29 10:55:08 -0400

    cambios yulix subidos

--- cliente.c
+++ cliente.c
@@ -6,71 +6,81 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h>
+#include <dirent.h>
 
 int main(int argc, char const *argv[])
 {
     //cliente
     int id,asd; // el identificador retornado se usa en todas las funciones de los sockets
-    char buffer[100], bufferRes[100];
-    char opcion;
-    DIR dir;
-    struct dirent* dent;
-    if(argv[2] == NULL)
+    char opcion[2], bufferRes[2];
+    
+    if(argv[1] == NULL || argv[2] == NULL)
     {
-        printf("Error por falta de par�metros\n");
+        printf("Error por falta de parámetros\n");
         return 0;/*Se termina el programa*/
     }
-	
 
     struct sockaddr_in server;
     struct sockaddr *serv_addr;
     struct hostent *he;
-
-    if((he = gethostbyname(argv[1])) == NULL)
-        DieWithError("Error de gethostbyname().\n");
+    he = gethostbyname(argv[1]);
+    if (he==NULL)
+    {
+        printf("Error de gethostbyname().\n");
+        exit(-1);
+    }
 
     int puerto = strtoul(argv[2], NULL, 10);
-
     //Estructura del servidor
     server.sin_family = AF_INET;
     server.sin_port = htons(puerto);
-    server.sin_addr = *((struct in_addr *)he->h_addr);
-
+    server.sin_addr = *((struct in_addr *)he->h_addr); 
     bzero(&(server.sin_zero), 8);
 
-	printf("MENU\n");
-	printf("1) COMPROBAR ARCHIVOS\n");
-	printf("2) AGREGAR ARCHIVO\n");
-	printf("3) ELIMINAR ARCHIVO\n");
-	printf("4) SALIR\n");
-	
-	
+    //Menu 
+    printf("MENU\n");
+    printf("1) COMPROBAR ARCHIVOS\n");
+    printf("2) AGREGAR ARCHIVO\n");
+    printf("3) ELIMINAR ARCHIVO\n");
+    printf("4) SALIR\n");
+
     //Socket cliente
     while(1)
-    {
-		printf("ESCOJA UNA OPCION: ")
-		scanf("%c", &opcion);
+    {   
         //Creamos el socket
         id = socket(AF_INET, SOCK_STREAM, 0);
-        //scanf("%*c%[^\n]", buffer);
+
+        //Opción del menu
+        printf("ESCOJA UNA OPCION: ");
+        scanf("%s", opcion);
+
         //Nos conectamos al servidor
-        if(connect(id, (struct sockaddr *) &server, sizeof(struct sockaddr)) == -1) DieWithError("connect() error\n");
-        //write(id, buffer, 100);
-        send(id, opcion, 1, 0);
-        if(strcmp(buffer,"exit") == 0)
+        if(connect(id, (struct sockaddr *)&server, sizeof(struct sockaddr))==-1)
         {
-            printf("Se termina el ciclo.\n");
+             printf("Error en connect()\n");
+             exit(-1);
+        }
+
+        //write(id, opcion, 2);
+        send(id, opcion, 2, 0);
+        
+        if(strcmp(opcion,"4") == 0)
+        {
+            printf("Se termina el ciclo.\n"); 
             break;
         }
-        if ((asd =recv(id, bufferRes, 100, 0)) == -1)
-            DieWithError("Error en recv() \n");
-       
+        
+        if ((asd =recv(id, bufferRes, 2, 0)) == -1)
+        {
+             printf("Error en recv() \n");
+             exit(-1);
+        }
+        
         printf("\nRespuesta del servidor: %s\n\n", bufferRes);
-
+        
     }
-
+    
     close(id);
 
     return 0;
 }
-
--- server.c
+++ server.c
@@ -6,73 +6,109 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h>
-#include <arpa/inet.h>
-
+#include <dirent.h>
 
 int main(int argc, char const *argv[])
 {
     int id, id_new, asd; // el identificador retornado se usa en todas las funciones de los sockets
     int tam;
-    char buffer[100], bufferRes[100];
+    char opcion[2], bufferRes[200];
+    
     if(argv[1] == NULL)
-        DieWithError("Error por falta de parametros.\n")
+    { 
+        printf("Error por falta de parametros.\n");
+        return 0;
+    }
 
     int puerto = strtoul(argv[1], NULL, 10);
     struct sockaddr_in server;//Infromacion del servidor
     struct sockaddr_in client;//Infromacion del cliente
-	
+
     //Estructura server del servidor
     server.sin_family = AF_INET;
     server.sin_addr.s_addr = INADDR_ANY;
     server.sin_port = htons(puerto); // puerto donde escucha
-    bzero(&(server.sin_zero), 8);
+    bzero(&(server.sin_zero),8);
 
     //Socket del servidor
     id = socket(AF_INET, SOCK_STREAM, 0);
 
-    bind(id, (struct sockaddr*)&server, sizeof(struct sockaddr));
+    bind(id, (struct sockaddr*)&server, sizeof(struct sockaddr)); 
     listen(id, 5);
 
     while(1)
     {
         tam = sizeof(struct sockaddr_in);
-        if ((id_new = accept(id,(struct sockaddr *)&client,&tam)) == -1)
-            DieWithError("error en accept()\n");
-        
+    
+        if ((id_new = accept(id,(struct sockaddr *)&client,&tam))==-1) 
+        {
+         printf("error en accept()\n");
+         exit(-1);
+        }
+
         printf("Se obtuvo una conexión desde %s\n", inet_ntoa(client.sin_addr));
-		
-		//read(id_new, buffer, 100);
-        f ((asd =recv(id_new, opcion, 1, 0)) == -1)
-            DieWithError("Error en recv() \n")
-		
+        //read(id_new, opcion, 2);
+        
+        if ((asd =recv(id_new, opcion, 2, 0)) == -1)
+        {
+            printf("Error en recv() \n");
+            exit(-1);
+        }
+        
         if(strcmp(opcion,"4") == 0)
         {
-            printf("Se termina el ciclo.\n");
+            printf("Se termina el ciclo.\n"); 
             break;
-		}
-		printf("Se selecciona %c", opcion);
-		
-		switch(opcion){
-			case '1':
-				cosasArrechisimas();
-			break;
-			case '2':
-			break;
-			case '3':
-			break;
-			default:
-			break;
+        }
 
-		}
-        //printf("\nMensaje del cliente: %s\n\n", buffer);
+        printf("\nMensaje del cliente: %s\n\n", opcion);        
+        
+        if(strcmp(opcion,"1") == 0) 
+        {
+            /*ignora esto(??*/
+            struct dirent* dent;
+            DIR *dirp;
+            int contDir = 0, contArch = 0;
+            char* path = "./client";
+            dirp = opendir(path);
 
+            while(1)
+            {
+                dent = readdir(dirp);
 
-		strcpy(bufferRes, "Mensaje recibido");
-		
-        send(id_new, bufferRes, 100,0);
+                if (dent == NULL) break;
+                if (dent->d_type == DT_DIR)
+                {   
+                    contDir++;
+                }
+                else if (dent->d_type == DT_REG)
+                {
+                    contArch++;
+                }
+            }
+
+            printf("Cantidad de directorios: %d\n", contDir);
+            printf("Cantidad de archivos: %d\n", contArch);
+        }
+        else if(strcmp(opcion,"2") == 0) 
+        {
+            strcpy(bufferRes,"Esta opción aun no está implementada.\n");      
+        }
+        else if(strcmp(opcion,"3") == 0) 
+        {
+            strcpy(bufferRes,"Esta opción aun no está implementada.\n");
+        }
+        else 
+        {
+            strcpy(bufferRes, "Esta opción aun no está implementada.\n");
+        }
+        
+        send(id_new, bufferRes, 200,0);
 
         close(id_new);
     }
+    
+    
     close(id);
     return 0;
 }
