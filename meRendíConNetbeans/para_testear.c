dbc340ab50e14177aec27a86d9017580e72bf343
adrianjmejias <adrianjmejias@gmail.com>
2018-05-02 21:44:20 -0400

    segmentFile ready

--- meRendíConNetbeans/para_testear.c
+++ meRendíConNetbeans/para_testear.c
@@ -1,7 +1,16 @@
-
+#include "includes/todos_los_includes.h"
+#include "includes/colaT.h"
+#include "includes/segmentFile.h"
 
 
 
 int main(){
-    
+    Cola *a = segmentFile("includes/segmentFile.h");
+    int tam = a->tamanio;
+    Leer(a);
+
+    while(!colaVacia(a)){
+        printf(Desencolar(a).path);
+    }
+    printf("\ntamanio %d\n", tam);
 }
